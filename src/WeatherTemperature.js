412f6bbbabf21fc6783d56544f0bd7694d83c593
Maria Sharova <sharova.maria.3d@gmail.com>
2022-12-26 19:39:59 +0100

    added the Celsius/Fahrenheit conversion

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -4,24 +4,67 @@ import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 
 export default function WeatherTemperature(props) {
-  return (
-    <span className="WeatherTemperature">
-      <div className="d-flex weather-temperature">
-        <Row>
-          <Col lg={4} xs={4}>
-            <img src={props.icon} alt={props.description} />
-          </Col>
-          <Col lg={4} xs={4} className="text-right">
-            <span className="temperature">{Math.round(props.celsius)}</span>
-          </Col>
-          <Col lg={4} xs={4} className="units">
-            <a href="./" className="active">
-              °C
-            </a>
-            |<a href="./">°F</a>
-          </Col>
-        </Row>
-      </div>
-    </span>
-  );
+  const [unit, setUnit] = useState("celsius");
+
+  function convertToFahrenheit(event) {
+    event.preventDefault();
+    setUnit("fahrenheit");
+  }
+  function convertToCelcius(event) {
+    event.preventDefault();
+    setUnit("celsius");
+  }
+  function fahrenheit() {
+    return (props.celsius * 9) / 5 + 32;
+  }
+
+  if (unit === "celsius") {
+    return (
+      <span className="WeatherTemperature">
+        <div className="d-flex weather-temperature">
+          <Row>
+            <Col lg={4} xs={4}>
+              <img src={props.icon} alt={props.description} />
+            </Col>
+            <Col lg={4} xs={4} className="text-right">
+              <span className="temperature">{Math.round(props.celsius)}</span>
+            </Col>
+            <Col lg={4} xs={4} className="units">
+              <span href="./" className="active">
+                °C
+              </span>
+              |
+              <a href="./" onClick={convertToFahrenheit}>
+                °F
+              </a>
+            </Col>
+          </Row>
+        </div>
+      </span>
+    );
+  } else {
+    return (
+      <span className="WeatherTemperature">
+        <div className="d-flex weather-temperature">
+          <Row>
+            <Col lg={4} xs={4}>
+              <img src={props.icon} alt={props.description} />
+            </Col>
+            <Col lg={4} xs={4} className="text-right">
+              <span className="temperature">{Math.round(fahrenheit())}</span>
+            </Col>
+            <Col lg={4} xs={4} className="units">
+              <a href="./" onClick={convertToCelcius}>
+                °C
+              </a>
+              |
+              <span href="./" className="active">
+                °F
+              </span>
+            </Col>
+          </Row>
+        </div>
+      </span>
+    );
+  }
 }
